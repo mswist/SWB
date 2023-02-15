3f26885c51fbb2eb3f449d7be06203d311337a29
mswist <mswist@users.noreply.github.com>
2023-02-15 18:37:56 +0100

    directory listing

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ async function getDirectoriesFromGit(url) {
   return response.json();
 }
 
+listDirectories(gitContent) {
+  const directories = gitContent.filter(el => el.type == "dir")
+  directories.forEach(dir => displayDirElement)
+}
+
+function displayDirElement(dir) {
+  const href = document.createElement("a")
+  href.textContent = dir.name
+  href.href = `./${dir.path}`
+  document.body.insertAdjacentElement("afterbegin", href)
+}
+
+
 getDirectoriesFromGit("https://api.github.com/repos/mswist/SWB/contents")
-  .then(resp => console.log(resp))
+  .then(resp => listDirectories(resp))
